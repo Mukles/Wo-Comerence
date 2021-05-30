c4761924f4a48a29d216dbc2208dfd60b96561c4
Mukles Hossen <Mukleshossen1763@gmail.com>
2021-05-30 10:55:23 +0600

    including some commments

--- WebApplication2/Startup.cs
+++ WebApplication2/Startup.cs
@@ -48,6 +48,7 @@ namespace WebApplication2
             });
         }
 
+        //there is some chnages
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
